77ac472f8c4d2e1aa2ea4448e235872c864e5346
Max <max30134@gmail.com>
2020-05-04 15:39:23 +0300

    adapt 1st page v2.2

--- ksenmart/src/js/partials/sliders.js
+++ ksenmart/src/js/partials/sliders.js
@@ -12,27 +12,30 @@ $(document).ready(function(){
         nextArrow: '<button class="next arrow"></button>',
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1200,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
                     dots: false,
-                    arrows: false
+                    arrows: true
                     
                 }
             },
+
+            
+
             {
-                breakpoint: 600,
+                breakpoint: 992,
                 settings: {
-                // slidesToShow: 3,
-                // slidesToScroll: 1,
-                infinite: false,
-                speed: 300,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                // centerMode: true,
-                variableWidth: true,
-                arrows: false
+                    // slidesToShow: 3,
+                    // slidesToScroll: 1,
+                    infinite: false,
+                    speed: 300,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    // centerMode: true,
+                    variableWidth: true,
+                    arrows: false
                 }
             }
 
